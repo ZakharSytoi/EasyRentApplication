3f3a9ed41285b9b4ba47155406b0ca885792daef
ZakharSytoi <zakharsytoi@gmail.com>
2024-01-12 17:39:38 +0100

    error with page fixed

--- src/main/java/com/example/deploydemo/repository/daos/ApartmentRepository.java
+++ src/main/java/com/example/deploydemo/repository/daos/ApartmentRepository.java
@@ -2,17 +2,17 @@ package com.example.deploydemo.repository.daos;
 
 import com.example.deploydemo.repository.model.Apartment;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ApartmentRepository extends JpaRepository<Apartment, Long> {
     @Query("SELECT a FROM Apartment a WHERE a.owner.id = ?1")
-    Page<Apartment> findAllByUserId(Long userId);
+    Page<Apartment> findAllByUserId(Long userId, Pageable pageable);
 
     @Query("SELECT a FROM Apartment a WHERE a.id=?1 AND a.owner.id = ?2")
     Optional<Apartment> findByIdAndUserId(Long id, Long userId);
