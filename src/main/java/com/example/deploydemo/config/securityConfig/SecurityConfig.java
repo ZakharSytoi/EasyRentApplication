6c5575000d36fa79045ecbc6c5369b8b7e71e412
ZakharSytoi <zakharsytoi@gmail.com>
2024-01-12 14:57:45 +0100

    swagger security configured. cors configured

--- src/main/java/com/example/deploydemo/config/securityConfig/SecurityConfig.java
+++ src/main/java/com/example/deploydemo/config/securityConfig/SecurityConfig.java
@@ -1,15 +1,15 @@
-package com.example.deploydemo.securityconfig;
+package com.example.deploydemo.config.securityConfig;
 
-import com.example.deploydemo.service.UserService;
 import com.example.deploydemo.service.security.JwtRequestFilter;
 import com.example.deploydemo.service.security.UserDetailServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,9 +19,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 import static com.example.deploydemo.service.security.Permission.*;
 
@@ -30,6 +33,9 @@ import static com.example.deploydemo.service.security.Permission.*;
 @EnableMethodSecurity
 @Configuration
 public class SecurityConfig {
+
+    @Value("${environment.allowedOrigins}")
+    private String allowedOrigins;
 
     private final JwtRequestFilter jwtRequestFilter;
     @Bean
@@ -54,11 +60,30 @@ public class SecurityConfig {
     }
 
     @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(allowedOrigins.split(",")));
+        //configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:8077", "http://localhost:80", "http://3.70.177.167"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(
+                            "/swagger-ui.html",
+                            "/v3/api-docs",
+                            "/v3/api-docs/**",
+                            "/swagger-ui/**"
+                    ).permitAll();
                     auth.requestMatchers("/easyrent-api/v1/login").permitAll();
                     auth.requestMatchers("/easyrent-api/v1/register_owner").permitAll();
                     auth.requestMatchers("/easyrent-api/v1/register_tenant").hasAuthority(TENANT_USER_CREATE.toString());
