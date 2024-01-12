106ff65494bb346bbe4e5ea84b52d2325f1ad669
ZakharSytoi <zakharsytoi@gmail.com>
2024-01-12 15:32:25 +0100

    garbage removed

--- src/main/java/com/example/deploydemo/web/controllers/ResponseApartmentObject.java
+++
@@ -1,9 +0,0 @@
-package com.example.deploydemo.web.controllers;
-
-public record ResponseApartmentObject(
-        String name,
-        Long ownerId,
-        String address,
-        String note
-) {
-}
