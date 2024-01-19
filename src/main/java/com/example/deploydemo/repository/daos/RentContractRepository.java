30df6db28cfdccf4c4fec29c2108325f18f3e2ea
ZakharSytoi <zakharsytoi@gmail.com>
2024-01-19 08:13:45 +0100

    method for finding RentContract by residentUser_id added

--- src/main/java/com/example/deploydemo/repository/daos/RentContractRepository.java
+++ src/main/java/com/example/deploydemo/repository/daos/RentContractRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RentContractRepository extends JpaRepository<RentContract, Long> {
     Page<RentContract> getRentContractsByApartment_Id(Long id, Pageable pageable);
+    Optional<RentContract> findByResidentUser_Id(Long id);
 }
